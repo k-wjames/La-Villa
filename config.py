d6ad5a2714c1ae4556c3f51a25e00f78ed018d16
k-wjames <kwjames9326@gmail.com>
2026-02-08 22:05:22 +0300

    Fix indentation in config.py

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "reservations.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-      SECRET_KEY = "your-secret-key"
-
+    SECRET_KEY = "your-secret-key"
     # Flask-Mail settings
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
